26e236f0562247c2ce8f3290a53b94d89e33b548
김경윤 <l982930009@gmail.com>
2025-09-23 17:57:54 +0900

    feat: 회원가입/로그인

---
+++ src/main/java/com/GoWalk/global/security/MemberDetailsServicce.java
@@ -0,0 +1,22 @@
+package com.GoWalk.global.security;
+
+import com.GoWalk.domain.member.application.entity.Member;
+import com.GoWalk.domain.member.application.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailsServicce implements UserDetailsService {
+	private final MemberRepository memberRepository;
+
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Member member = memberRepository.findByUsername(username).orElseThrow(()
+				-> new UsernameNotFoundException("유저정보를 찾을 수 없습니다."));
+
+		return new MemberDetails(member);
+	}
+}
