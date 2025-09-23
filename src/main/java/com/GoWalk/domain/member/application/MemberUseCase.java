3f45a14b5bfbd7700338cf49798a292bdc602bde
daehyeong2 <baconbacon1231@gmail.com>
2025-09-23 16:53:03 +0900

    feat: /api/members/profile 추가

--- src/main/java/com/GoWalk/domain/member/application/MemberUseCase.java
+++ src/main/java/com/GoWalk/domain/member/application/MemberUseCase.java
@@ -1,12 +1,18 @@
 package com.GoWalk.domain.member.application;
 
 import com.GoWalk.domain.member.application.data.MockMember;
+import com.GoWalk.domain.member.application.data.res.GetMyInfoRes;
+import com.GoWalk.domain.member.application.data.res.GetMyProfile;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MemberUseCase {
-  public MockMember getMyInfo() {
-    return new MockMember("gorani", "hello world");
+  public GetMyInfoRes getMyInfo() {
+    return GetMyInfoRes.of(new MockMember("gorani", "legend"));
+  }
+
+  public GetMyProfile getMyProfile() {
+    return GetMyProfile.of(new MockMember("고rani", "legend"));
   }
 }
 
