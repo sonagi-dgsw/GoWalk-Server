1c7cfb85b4eedec8cc167422b92533e90857c604
김경윤 <l982930009@gmail.com>
2025-09-25 10:20:04 +0900

    fix: 토큰 발급시 생기던 버그 수정, MemberAuthHolder 추가

--- src/main/java/com/GoWalk/domain/member/presentation/MemberController.java
+++ src/main/java/com/GoWalk/domain/member/presentation/MemberController.java
@@ -1,12 +1,11 @@
 package com.GoWalk.domain.member.presentation;
 
 import com.GoWalk.domain.member.application.MemberUseCase;
-import com.GoWalk.domain.member.application.data.res.GetMyInfoRes;
+import com.GoWalk.domain.member.application.data.MockMember;
 import com.GoWalk.domain.member.application.data.res.GetMyProfile;
 import com.GoWalk.global.data.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +16,7 @@ public class MemberController {
   private final MemberUseCase memberUseCase;
 
   @GetMapping("/me")
-  public ApiResponse<GetMyInfoRes> me() {
+  public ApiResponse<MockMember> me() {
     return ApiResponse.ok(memberUseCase.getMyInfo());
   }
 
