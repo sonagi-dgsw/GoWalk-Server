edcae230cc7ee63ddb9ac35bbc733b1191b3568f
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-09-30 20:09:43 +0900

    feat: define WalkRouteRes DTO with places and rating

--- src/main/java/com/GoWalk/domain/walk/application/data/response/WalkRouteRes.java
+++ src/main/java/com/GoWalk/domain/walk/application/data/response/WalkRouteRes.java
@@ -1,13 +1,8 @@
 package com.GoWalk.domain.walk.application.data.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
-public class WalkRoute {
-    private List<String> places;  // 경로에 포함된 장소들
-    private int rating;           // 경로 별점
-}
+public record WalkRouteRes(
+        List<String> places, // 경로에 포함된 장소들
+        int rating // 경로의 별점
+) {}
